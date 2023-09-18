be32762888bb82bdc76d99e19432efb14fdb2baf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-18 11:39:59 -0400

    day01

---
+++ day01/operators.py
@@ -0,0 +1,36 @@
+# arithmetic: +, -, *, /, %
+
+print(10 - 2)
+print(10 + 2)
+
+print(10 * 3)
+
+print(10 / 3)
+
+print(10 / 2)
+
+print(10 % 3)
+
+print(int(100 / 2))
+
+
+# shorthand assignment operators: =, +=, -=, *=, /=, %=
+
+a = 100
+
+a = 200
+
+print(a)
+
+
+a += 100
+
+print(a)
+
+a -= 50
+
+print(a)
+
+a /= 2
+
+print(a)
