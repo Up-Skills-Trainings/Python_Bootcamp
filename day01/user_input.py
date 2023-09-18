c7c842f16f9db55d2108a733f002eaa1825d3c53
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-18 14:39:17 -0400

    day01

---
+++ day01/user_input.py
@@ -0,0 +1,20 @@
+
+
+#print(  help(input)  )
+
+name = input('Enter your name:\n')
+age = int( input('Enter your age:\n') )
+gpa = float(input('Enter your gpa:\n'))
+boolean_value =  bool(input('Enter True or False:\n'))
+
+print(name)
+print( type(name) )
+
+print( age )
+print( type(age) )
+
+print(gpa)
+print( type(gpa))
+
+print(boolean_value)
+print( type(boolean_value))
