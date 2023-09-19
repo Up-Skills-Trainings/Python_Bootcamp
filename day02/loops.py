d1cd86de39d7f8f2586275b1c7ceb53bb0fca010
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-19 13:27:08 -0400

    day02

--- day02/loops.py
+++ day02/loops.py
@@ -18,3 +18,35 @@ print('-----------------------')
 
 for i in reversed( range(0, len(s)) ):
     print(s[i])
+
+print('-----------------------')
+
+result = ''
+
+for x in s[::-1]:
+   result += x
+
+print(result)
+
+print('-----------------------')
+
+for x in range(1, 11):
+    print('Hello World')
+
+print('-----------------------')
+
+num = int( input('Enter a positive number:\n'))
+
+while num <= 0:
+    num = int( input('Enter a positive number:\n'))
+
+print(f'You have entered {num}')
+
+print('-----------------------')
+
+answer = input('Enter yes or no:\n')
+
+while not ( answer.lower() == 'yes' or answer.lower() =='no'):
+    answer = input('Enter yes or no:\n')
+
+print(f'You have entered {answer}')
