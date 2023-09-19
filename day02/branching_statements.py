995456b36708b6a292a8ca8fba98f4efb9f30f74
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-19 13:32:46 -0400

    day02

---
+++ day02/branching_statements.py
@@ -0,0 +1,20 @@
+
+for i in range(1, 6):
+    print(i)
+    if i == 3:
+        break # exits the loop
+
+print('-------------')
+
+for i in range(1, 6):
+    if i == 3 or i == 4:
+        continue # skips to the next iteration
+    print(i)
+
+print('-------------')
+
+
+for i in range(1, 6):
+    if i == 3 or i == 4:
+        pass
+    print(i)
