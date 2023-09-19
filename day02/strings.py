4f1db061a40029b1e6869261d87f9ec3b83b2932
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-19 10:43:48 -0400

    day01

---
+++ day02/strings.py
@@ -0,0 +1,54 @@
+name = 'Python'
+
+print(len(name))
+print(name[0])
+print(name[len(name) - 1])
+
+print(name[-1])
+print(name[-2])
+
+s = 'Java Programming Language'
+s2 = s[5:16]
+
+print(s2)
+
+s3 = s[:4]
+
+print(s3)
+
+s4 = s[::-1]  # reverses the string after slicing
+
+print(s4)
+
+s = 'Python Programming'
+
+s5 = str(reversed(s))
+
+print(type(s5))
+reversed(s5)
+
+print(s5)
+
+s5 = s[::-1]
+
+print(s5)
+
+s5 = 'python Programming'
+
+s6 = s5[7:]  # by default it slices the string from index 7 to the last character
+
+print(s6)
+
+
+s7 = 'CYDEO SCHOOL'
+# str(reversed(s7))
+
+print('----------------------')
+
+# print( help(str))
+
+
+print('----------------------')
+
+
+
