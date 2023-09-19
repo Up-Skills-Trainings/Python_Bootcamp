4c4b004e78c7e7a69d86593b95701a2569d8ed98
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-19 12:40:20 -0400

    day01

---
+++ day02/utility.py
@@ -0,0 +1,21 @@
+import numbers
+
+
+def concat(a: str, b, c='', d='', e=''):
+    print(f'{a} {b} {c} {d} {e}'.strip())
+
+def sum(n1: numbers, n2: numbers, n3: numbers = 0, n4: numbers = 0) -> numbers:
+    return n1 + n2 + n3 + n4
+
+def calculate(num1: numbers, num2: numbers, operator: str) -> numbers:
+    if operator == '-':
+        return num1 - num2
+    elif operator == '+':
+        return num1 + num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        print('Invalid operator')
+        return 0
