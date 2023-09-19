b1d11cf9f1f486093f3a3810392bac8439a39cf6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-19 11:36:58 -0400

    day01

---
+++ day02/functions.py
@@ -0,0 +1,56 @@
+
+"""
+
+Java Methods:
+    public static void method(parameter){
+
+    }
+
+
+"""
+import numbers
+
+
+def display_message():
+    print('Hello Cydeo')
+    print('Hello World')
+
+
+display_message()
+
+def value():
+    return 'Python Programming'
+
+print( value() )
+
+
+def return_int() -> int:
+    return 10.0
+
+print(  return_int() )
+
+
+def square(num: int):
+    return num * num
+
+
+print( square(10) )
+# print( square('Java'))
+
+# print( divide('C#', 'Python'))
+
+def add(num1: numbers, num2: numbers) -> numbers:
+    return num1 + num2
+
+
+print(add(10, 20))
+
+print(add(10.5, 20.5))
+
+
+print('------------------------------------------')
+
+
+def calculate() -> numbers:
+
+
