ff8376a0a953840216968c453da193a98ed70d97
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-20 12:52:55 -0400

    day03

--- day03/encapsulation2.py
+++ day03/encapsulation2.py
@@ -1,10 +1,10 @@
-
 class Person:
 
-    def __init__(self, name):
+    def __init__(self, name: str = 'Jimmy', age: int = 2):
         self.__name = None
         self.__age = None
         self.person_name = name
+        self.person_age = age
 
     @property
     def person_name(self):
@@ -15,7 +15,6 @@ class Person:
 
     @person_name.setter
     def person_name(self, name):
-
         if type(name) != str:
             raise RuntimeError(f'Person name must be string')
 
@@ -23,3 +22,23 @@ class Person:
             raise RuntimeError(f'Person name can not be empty')
 
         self.__name = name
+
+    @property
+    def person_age(self):
+        return self.__age
+
+    @person_age.setter
+    def person_age(self, age):
+        if age < 0 or age > 150:
+            raise RuntimeError(f'Inavlid age {age}')
+
+        self.__age = age
+
+    def __str__(self):
+        return f'{type(self).__name__}{str(self.__dict__).replace("_Person__", "")}'
+
+
+person1 = Person()
+
+person1.person_name = "Daniel"
+print(person1.person_name)
