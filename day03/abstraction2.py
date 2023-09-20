3afeeec310798c624b55bd66dd7885c76a07cd6c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-20 15:25:24 -0400

    day03

--- day03/abstraction2.py
+++ day03/abstraction2.py
@@ -1,6 +1,7 @@
 import numbers
 
 from abc import ABC, abstractmethod
+
 # import abc
 
 """
@@ -8,6 +9,14 @@ abc: module name (built in module)
 ABC: Abstract class in abc module
 abstractmethod: annotations that can be given to abstract methods
 """
+
+
+class Volume(ABC):
+
+    @abstractmethod
+    def volume(self):
+        pass
+
 
 class Shape(ABC):
 
@@ -20,3 +29,49 @@ class Shape(ABC):
 
     def __str__(self):
         return f'{type(self).__name__}{self.__dict__}'
+
+
+class Square(Shape):
+
+    def __init__(self, side):
+        super().__init__()
+        self.side = side
+
+    def area(self) -> numbers:
+        return self.side * self.side
+
+
+class Circle(Shape):
+
+    def area(self) -> numbers:
+        pass
+
+
+class Rectangle(Shape):
+
+    def area(self) -> numbers:
+        pass
+
+
+class Cube(Shape, Volume):
+
+    def area(self) -> numbers:
+        pass
+
+    def volume(self):
+        pass
+
+
+class Cylinder(Shape, Volume):
+
+    def area(self) -> numbers:
+        pass
+
+    def volume(self):
+        pass
+
+
+square1 = Square(5)
+
+print(square1)
+print(square1.area())
