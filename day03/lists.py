e3fadc4203767dc64715d5af51be62f547e07134
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-20 11:38:00 -0400

    day03

--- day03/lists.py
+++ day03/lists.py
@@ -174,8 +174,34 @@ for x in range(1, 51):
 
 print(nums)
 
+print('------------------------------------')
 
+"""
+divisible_by_5 = []
 
+for x in nums:
+    if x % 5 == 0:
+        divisible_by_5.append(x)
 
+print(divisible_by_5)
+"""
 
+divisible_by_5 = tuple( [ x  for x in nums if x % 5 == 0 ] )
 
+print(divisible_by_5)
+
+print('------------------------------------')
+
+even_nums = [ x for x in nums if x % 2 == 0]
+odd_numbers = [x for x in nums if x % 2 != 0]
+
+print(even_nums)
+print(odd_numbers)
+
+print('------------------------------------')
+
+names = ['Python', 'Java', 'Java', 'JavaScript', 'java', 'JaVA', 'Ruby']
+
+names = [x for x in names if x.lower() != 'java']
+
+print(names)
