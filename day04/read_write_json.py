59066e486eb23f77b572ef46303b5aee96d89543
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-21 15:04:19 -0400

    day04

---
+++ day04/read_write_json.py
@@ -0,0 +1,50 @@
+import json
+
+
+path = 'files/Test.json'
+
+jason_file = open(path, 'r')
+
+dictionary = json.load(jason_file)
+
+print(dictionary)
+print(type(dictionary))
+
+for x in dict(dictionary).keys():
+    print(x)
+
+jason_file.close()
+
+print('------------ Write Json file----------------------')
+
+
+students = {
+    'A01': {
+        'name': 'James',
+        'gender': 'Male',
+        'gpa': 3.5,
+        'subjects': ['Math', 'Physics']
+    },
+
+    'A02': {
+        'name': 'Hazel',
+        'gender': 'Female',
+        'gpa': 3.8,
+        'subjects': ['Biology', 'Programming']
+    },
+
+    'A03': {
+        'name': 'Yulia',
+        'gender': 'Female',
+        'gpa': 4,
+        'subjects': ['Chemistry', 'Programming']
+    }
+}
+
+jason_file = open('files/Test2.json', 'w')
+
+json_object = json.dumps(students, indent=3)   # converting dictionary object to json object
+
+jason_file.write(json_object)
+
+
