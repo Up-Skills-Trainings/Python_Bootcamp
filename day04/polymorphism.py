37a425d290460c71d490135f4a4aa44328a0daff
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-21 10:44:27 -0400

    day04

---
+++ day04/polymorphism.py
@@ -0,0 +1,20 @@
+from day03.abstraction2 import Shape, Square, Rectangle
+from day03.inheritance import Person
+
+shape1: Shape = Square(5)
+
+shape2: Shape = Rectangle(3, 4)
+
+
+def display_area(shape: Shape):  # parameter's type is restricted to shape objects ONLY
+    print(f'the  {shape.name}\' area is {shape.area()} ')
+
+
+display_area( shape1 )
+display_area( shape2)
+
+
+person1 = Person('James', 35)
+
+
+
