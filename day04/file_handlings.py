dd0c7b814dbc966d1c944379a96f195c6e05a9cf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-21 13:13:14 -0400

    day04

--- day04/file_handlings.py
+++ day04/file_handlings.py
@@ -13,3 +13,13 @@ print(text_file.readline())
 print(text_file.readline())
 """
 
+print('----------------------------------')
+
+path = 'files/Test2.txt'
+
+text_file2= open(path, 'w')
+
+text_file2.write('This is a new text file\nPython created this file')
+
+text_file2.close()
+
