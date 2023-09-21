a776edb9b4cc9851c2b900b92b40e78aed23507f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2023-09-21 13:09:56 -0400

    day04

---
+++ day04/file_handlings.py
@@ -0,0 +1,15 @@
+path = 'files/Test.txt'
+
+text_file = open(path, 'r')
+
+print( text_file.read() )
+
+
+text_file.close()
+
+"""
+print(text_file.readline())
+print(text_file.readline())
+print(text_file.readline())
+"""
+
